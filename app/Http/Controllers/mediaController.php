a6aea5f0f4434c369558beaf72edd6c07644e08d
Paul Allen <p.allen.art@gmail.com>
2018-09-04 23:13:23 -0600

    Check uploaded file type.

--- app/Http/Controllers/mediaController.php
+++ app/Http/Controllers/mediaController.php
@@ -14,11 +14,17 @@ class MediaController extends Controller
 
     public function create(Request $request)
     {
+        $file = $request->file('file');
+        $file_type = $file->extension();
 
+        // Check file type for images
+        if (!preg_match('/(jpg|jpeg|png|gif)$/i', $file_type)) {
+            return response('File MIME Type not Allowed', 422);
+        }
 
         // Get the file from the request, and store it
         // $path represents where the file is in the application
-        $path = Storage::putFile('public', $request->file('file'));
+        $path = Storage::putFile('public', $file);
 
         // Return the asset url
         // Storage::url will return the public url path of the file
